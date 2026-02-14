aea22916dbb8cf83701ee6df2d9c84f850d378a8
Josep Ocaña <j.ocanapuigdevall@gmail.com>
2026-02-14 16:48:23 +0100

    feat(backend): add root GET route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,13 @@ import express from "express";
 // Creamos la aplicación de Express
 const app = express();
 
+// Middleware
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Servidor funcionando" });
+});
+
 // El servidor escucha un puerto
 app.listen(3000, () => {
   console.log("🚀 Servidor corriendo en http://localhost:3000");
