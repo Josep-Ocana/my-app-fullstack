1ce13dcca61ec1b2ce73b842a6654b5e46e9d9ce
Josep Ocaña <j.ocanapuigdevall@gmail.com>
2026-02-18 15:50:48 +0100

    feat(backend): add email and phone fields to user model

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -3,6 +3,8 @@ import z from "zod";
 
 export const UserSchema = z.object({
   name: z.string().min(3, "El nombre debe tener al menos 3 caracteres"),
+  email: z.email("El email debe ser válido"),
+  phone: z.string().min(9, "El telefono debe tener al menos 9 dígitos"),
 });
 
 export type IUser = z.infer<typeof UserSchema>;
@@ -20,6 +22,20 @@ const userSchema = new Schema<IUserDocument>(
       required: [true, "El nombre es requerido"],
       trim: true,
       minLength: 3,
+    },
+    email: {
+      type: String,
+      required: [true, "El email es requerido"],
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    phone: {
+      type: String,
+      required: [true, "El teléfono es requerido"],
+      unique: true,
+      trim: true,
+      minLength: 9,
     },
   },
   { timestamps: true },
